81845bdb472c8f5946f8f54a2a298af18c8b793a
Eduardo Santoyo Castro <edusant@Eduardos-MacBook-Pro.local>
2022-08-22 10:25:23 -0500

    Código bueno

--- Prueba.java
+++ Prueba.java
@@ -1,20 +1,22 @@
 public class Prueba {
     public static int edit(String s1, String s2, int cont) {
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.length() == 0 || s2.length() == 0)
-              return cont + s1.length() + s2.length();
-            if (s1.charAt(i) == s2.charAt(i)) {
-              return edit(s1.substring(i, s2.substring(i)));
-            }
-            // tres posibles escenarios
-            int sus = edit(s1.substring(i), s2.substring(i), cont+1);
-            int ins = edit(s1.substring(i), s2, cont+1);
-            int bor = edit(s1, s2.substring(i), cont+1);
-            return min(sus, ins, bor);
-        }
+        if(s1.length()==0||s2.length()==0)
+            return cont+s1.length()+s2.length();
+        if(s1.charAt(0)==s2.charAt(0))
+            return edit(s1.substring(1), s2.substring(1), cont);
+        int sus=edit(s1.substring(1), s2.substring(1), cont+1);
+        int ins=edit(s1.substring(1), s2, cont+1);
+        int bor=edit(s1, s2.substring(1), cont+1);
+        int aux=Math.min(sus, ins);
+        return Math.min(aux, bor);
     }
 
     public static void main(String[] args) {
+
+        String s1 = "ernando";
+        String s2 = "Fernando";
+        int cont = 0;
+        System.out.println(Prueba.edit(s1, s2, cont));
 
     }
 }
