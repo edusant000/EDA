c4ce392493ff73ea430bbbcb7f59f4703d3ae251
Eduardo Santoyo Castro <edusant@Eduardos-MacBook-Pro.local>
2022-08-29 10:16:32 -0500

    nuevo Sort

--- EjSort.java
+++ EjSort.java
@@ -20,11 +20,44 @@ public class EjSort {
         }
         return arreglo;
     }
+
+    public static void imprimeNumerosNaturales(int n) {
+        
+        if (n < 50){ 
+            System.out.println(n);
+            imprimeNumerosNaturales(n+1);
+            System.out.println("no se alcanza este código" + (n+1));
+        }
+        System.out.println("definitivamente se alcanza este código" + (n+1));
+    }
+
+    public static int sumaNNumeros(int n) {
+        if (n > 0){
+            System.out.println(n + " " + (n-1));
+            return n + sumaNNumeros(n-1);
+        }
+        System.out.println("que pedooo" + n);
+        return n;
+    }
+
+    public static int fibonacci(int n) {
+        if (n > 0){
+            n = n-1;
+            return fibonacci();
+        }
+    }
+        
+
+    
     
     public static void main(String[] args) {
-        int[] arreglo = {2, 4, 1, 6, -1};
+        /*int[] arreglo = {2, 4, 1, 6, -1};
         arreglo = EjSort.bubbleSort(arreglo);
         for (int i = 0; i < arreglo.length; i++)
             System.out.println(arreglo[i]);
+    }*/
+    //EjSort.imprimeNumerosNaturales(0);
+    System.out.println(EjSort.sumaNNumeros(5) + "alvv");;
     }
+
 }
